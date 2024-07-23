a1ad969c756763cdc9f76f225c1d8ca35c39b8e4
Gean Lima <geansilvalima@gmail.com>
2024-07-22 21:15:46 -0300

    Ajuste no retorno de erro

--- CDBCalculatorApp/src/app/components/investment-calculator/investment-calculator.component.ts
+++ CDBCalculatorApp/src/app/components/investment-calculator/investment-calculator.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { InvestmentRequest, InvestmentService} from '../../services/investment.service';
+import { InvestmentRequest, InvestmentService } from '../../services/investment.service';
 import { InvestmentResult } from 'src/app/Models/InvestmentResult';
-
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-investment-calculator',
@@ -14,11 +14,36 @@ export class InvestmentCalculatorComponent {
     months: 0
   };
   investmentResult?: InvestmentResult;
+  errorMessage?: string;
 
   constructor(private investmentService: InvestmentService) { }
 
   calculateInvestment() {
     this.investmentService.calculateInvestment(this.investmentRequest)
-      .subscribe(result => this.investmentResult = result);
+      .subscribe(
+        result => {
+          this.investmentResult = result;
+          this.errorMessage = undefined;
+        },
+        (error: HttpErrorResponse) => {
+          console.log("error", error)
+          this.investmentResult = undefined;
+          if (error.status === 400) {
+            console.log("chegou", error.error.message)
+            this.errorMessage = error.error.message;
+          } else {
+            this.errorMessage = 'Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.';
+          }
+        }
+      );
+  }
+
+  resetForm() {
+    this.investmentRequest = {
+      initialValue: 0,
+      months: 0
+    };
+    this.investmentResult = undefined;
+    this.errorMessage = undefined;
   }
 }
